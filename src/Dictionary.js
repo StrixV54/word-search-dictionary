67dc6425ca04c58ebc9b88f8b54c95028938ff8d
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2023-04-23 15:37:12 +0530

    cors setup

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,23 +5,23 @@ import Results from "./Results";
 import Photos from "./Photos";
 import google from "googlethis";
 
+const options = {
+  page: 0,
+  safe: true, // Safe Search
+  parse_ads: false, // If set to true sponsored results will be parsed
+  additional_params: {
+    // add additional parameters here, see https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters and https://www.seoquake.com/blog/google-search-param/
+    hl: "en",
+  },
+};
+
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
   let [photos, setPhotos] = useState(null);
 
-  const options = {
-    page: 0,
-    safe: true, // Safe Search
-    parse_ads: false, // If set to true sponsored results will be parsed
-    additional_params: {
-      // add additional parameters here, see https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters and https://www.seoquake.com/blog/google-search-param/
-      hl: "en",
-    },
-  };
-
   function handleDictionaryResponse(response) {
-    console.log(response);
+    // console.log(response);
     // setResults(response.data[0]);
   }
 
@@ -38,13 +38,11 @@ export default function Dictionary() {
     //   .get(apiUrl, { headers: { "Content-Type": "" } })
     //   .then(handleDictionaryResponse);
     const fetchData = async () => {
-      fetch(
-        `https://google.com/search?q=define+${keyword}&ie=UTF-8&aomd=1&safe=active&start=0&hl=en`
-      )
-        .then((response) => response.json())
-        .then(handleDictionaryResponse);
+      const response = await google.search("TWDG", options);
+      console.log(response);
     };
     fetchData();
+
     // let pexelsApiKey =
     //   "563492ad6f917000010000016ae5ee198d504fc491478a0fefda2c43";
     // let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
