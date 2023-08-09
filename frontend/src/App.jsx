76dd09c68d9a3cf33dd544c13edea81c0b533d41
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2023-08-10 01:57:44 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/home.jsx";
+import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import About from "./pages/Login.jsx";
