a57230786f36834f34ac5777a8a5d2e9940a9528
Shrikant Vishwakarma <64365738+StrixV54@users.noreply.github.com>
2023-08-10 01:54:44 +0530

    updated url

--- frontend/src/utils/helper.jsx
+++ frontend/src/utils/helper.jsx
@@ -9,9 +9,11 @@ const getCurrentTime = new Date().toLocaleString(undefined, {
   year: "numeric",
   hour12: true,
 });
+// https://word-search-dictionary-dhgx0zx7s-strixv54.vercel.app/api
+// http://localhost:5000/api
 
 const axiosLocal = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://word-search-dictionary-dhgx0zx7s-strixv54.vercel.app/api",
 });
 
 export { getCurrentTime, axiosLocal, uuid };
